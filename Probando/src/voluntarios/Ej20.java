b6d287568b77aac6a810c92ce312457612aabbcd
mañana <maana@DESKTOP-N7K6P84>
2022-10-20 13:21:48 +0200

    ok

--- Probando/src/voluntarios/Ej20.java
+++ Probando/src/voluntarios/Ej20.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ej20 {
 
 	public static void main(String[] args) {
-		/*20. Realiza un programa que pida un número entero N entre 0 y 20 y luego muestre por pantalla
+		/* 20. Realiza un programa que pida un número entero N entre 0 y 20 y luego muestre por pantalla
 		los números desde 1 hasta N, uno en cada línea, repitiendo cada número tantas veces como su
 		valor. 
 		*/
